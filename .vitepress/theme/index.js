8fef86565d199f344c9ee94c67348625b1cbf966
player32611 <hcb326630@163.com>
2025-06-11 23:12:35 +0800

    初始化

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
-import DefaultTheme from '.vitepress/theme'
+import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
 /** @type {import('vitepress').Theme} */
